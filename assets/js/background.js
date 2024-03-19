2d380906b5cf3d7818bfa3f948fab444e5b825a9
davelai <dave.lai@kkyday.com>
2024-03-19 16:56:39 +0800

    feat: auto login

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,10 +1,94 @@
 chrome.webNavigation.onCompleted.addListener(
     (tab) => {
-        console.log('kk extension');
-        console.log(getDomain(tab.url));
+        if (!ifLoginPage(tab.url)) {
+            return;
+        }
+
+        let service = getService(tab.url);
+        const environment = getEnvironment(tab.url);
+        const account = accountInfo(environment, 'rbms');
+        switch (service) {
+            case 'rbms':
+                rbmsLogin(tab, account.username, account.password);
+                break;
+            case 'be2':
+                be2Login(tab, account.username, account.password);
+                break;
+        }
     }
 );
 
+function be2Login(tab, account, password) {
+    chrome.tabs.executeScript(tab.id, {
+        code: `
+            document.querySelector('input[type="email"]').value = '${account}';
+            document.querySelector('input[type="password"]').value = '${password}';
+            document.querySelector('button[type="button"]').click();
+        `
+    });
+}
+
+function rbmsLogin(tab, account, password) {
+    // <input type="text" class="form-control" placeholder="用戶名" name="username" value="">
+    // <input type="password" class="form-control" placeholder="密碼" name="password">
+    // <button type="submit" className="btn btn-primary btn-block btn-flat">登錄</button>
+
+    chrome.tabs.executeScript(tab.id, {
+        code: `
+            document.querySelector('input[name="username"]').value = '${account}';
+            document.querySelector('input[name="password"]').value = '${password}';
+            document.querySelector('button[type="submit"]').click();
+        `
+    });
+}
+
+function ifLoginPage(url) {
+    return url.includes('login');
+}
+
 function getDomain(url) {
     return url.replace('http://', '').replace('https://', '').split(/[/?#]/)[0];
+}
+
+function getEnvironment(url) {
+    return url.includes('sit') ? 'sit' : 'production';
+}
+
+function accountInfo(environment, $service) {
+    const accounts = {
+        'sit': {
+            'rbms': {
+                'username': 'sit-rbms-username',
+                'password': 'sit-rbms-password'
+            },
+            'be2': {
+                'username': 'sit-be2-username',
+                'password': 'sit-be2-password'
+            }
+        },
+        'production': {
+            'rbms': {
+                'username': 'prod-rbms-username',
+                'password': 'prod-rbms-password'
+            },
+            'be2': {
+                'username': 'prod-be2-username',
+                'password': 'prod-be2-password'
+            }
+        }
+    }
+
+    return accounts[environment][$service];
+}
+
+function getService(url) {
+    if (tab.url.includes('rbms')) {
+        return 'rbms';
+    }
+
+    if (tab.url.includes('be2')) {
+        return 'be2';
+    }
+
+    throw new Error('Service not found');
 }
