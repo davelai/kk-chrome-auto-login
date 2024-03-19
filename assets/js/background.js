a9e8614451fae0f5965b8a8035264622c042f554
davelai <dave.lai@kkyday.com>
2024-03-19 16:33:34 +0800

    coding style

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,11 +1,10 @@
-
 chrome.webNavigation.onCompleted.addListener(
     (tab) => {
         console.log('kk extension');
         console.log(getDomain(tab.url));
     }
-  );
+);
 
 function getDomain(url) {
-    return url.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+    return url.replace('http://', '').replace('https://', '').split(/[/?#]/)[0];
 }
